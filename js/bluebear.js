11ee29f79eb79a0621ff61a6e0acdd3989f4ce95
PC\USER <hiroyuki@netshop-biz.com>
2017-03-06 19:19:42 +0900

    データの挿入

---
+++ js/bluebear.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+<script>
+<!--基本形 -->
+$(function(){
+
+/*$("#mainimg").hide(2000);
+$("#mainimg").show(4000);
+$("#mainimg").hide('slow');
+$("#mainimg").fadeOut('2000');
+
+※fadeInは一度非表示にしないとダメ
+$("#mainimg").fadeIn('5000');
+
+※クリックしたら消える
+$("#mainimg").click(function(){
+   $("#mainimg").fadeOut('2000');
+   
+   
+});
+
+※マウスをおいたら動く
+$("#mainimg").mouseover(function(){
+   $("#mainimg").fadeOut('2000');
+});
+
+
+
+
+$("#mainimg").mouseover(function(){
+   $("#mainimg").attr('src', 'http://pictogram-free.com/material/001.png');
+});
+
+
+});
+*/
+
+</script>
